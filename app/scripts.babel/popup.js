acfae353ca2eb68d944bce1e6ac36968104e1f0b
Roland Saenz <r.saenz@roadiy.com>
2018-01-29 17:44:40 -0800

    initial commit

---
+++ app/scripts.babel/popup.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// $( "#searchBar" ).change(function() {
+//   search();
+// });
+//
+// function search() {
+//   var query = $('#searchBar').val();
+//
+//   $.ajax({
+//     type: 'GET',
+//     url: "https://bibles.org/v2/search.js?query=" + query,
+//     headers: { 'Authorization' : "Basic " + btoa("Dl5HoQUKdY55nfvKX8M8wc7LhRTNQx1KAX7lYW4V" + ":" + "X")},
+//     success: function (data) {
+//       $("#results").html(data);
+//       console.log("Results:", data);
+//     },
+//     error: function(){
+//       alert("Cannot get data");
+//     }
+//   })
+// }
